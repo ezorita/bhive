a3774eda6fa7b607fddc8c9ee00bbadefbe99993
Eduard Valera i Zorita <eduardvalera@gmail.com>
2017-11-16 13:55:23 +0100

    Fixes local clustering (supporting full chromosomes at 5kb). Changed GM to SVD+GM. Adds interchromosomal clustering algorithm.

--- hic/analysis/hic_basics.py
+++ hic/analysis/hic_basics.py
@@ -1,13 +1,16 @@
 from scipy import sparse
+import itertools
 import scipy
 import scipy.linalg
+import scipy.cluster
 import numpy as np
 import cooler
 import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import AxesGrid
-
-
+import os.path
+from sklearn import mixture, manifold
+from scipy.cluster.hierarchy import linkage,fcluster
+import pdb, traceback, sys
   
 def scale_resolution(matrix, factor):
     new_row = matrix.row // factor
@@ -19,14 +22,350 @@ def scale_resolution(matrix, factor):
     new_row = unique - new_col * (matrix.shape[0] // factor)
 
     return scipy.sparse.coo_matrix((sum, (new_row, new_col)))
+
+def center_diag(mat, offset):
+    d = np.diagonal(mat,offset)
+    if (np.isnan(d) == True).all():
+        return np.nan, np.nan
+    td = d[~np.isnan(d)]
+    if (td == 0).all():
+        return np.nan, np.nan
+    mean_val = np.mean(td)
+    td -= mean_val
+    span = np.max(td) - np.min(td)
+    td[np.where(td > 0)] /= np.max(td)
+    td[np.where(td < 0)] /= np.abs(np.min(td))
+    d.setflags(write=True)
+    d[~np.isnan(d)] = td
+    return mean_val, span
+
+def interchromosomal_clusters(cf,k,cluster_file):
+    clusters = {}
+    # Read and parse intrachromosomal clusters.
+    fc = open(cluster_file,"r")
+    for line in fc:
+        m = line.rstrip().split('\t')
+        chr = m[0]
+        clust = m[1].split(',')
+        if not clusters.has_key(chr):
+            clusters[chr] = []
+        clusters[chr].append(np.array([int(v) for v in clust]))
+
+    # Compute interchromosomal sums.
+    mat_shape = 0
+    offset = {}
+    for chr in clusters:
+        offset[chr] = mat_shape
+        mat_shape += len(clusters[chr])
+
+    mat    = np.zeros((mat_shape,mat_shape))
+    normat = np.zeros((mat_shape,mat_shape))
+    for i in xrange(0,len(clusters)):
+        for j in xrange(i+1,len(clusters)):
+            chr_i = clusters.keys()[i]
+            chr_j = clusters.keys()[j]
+            ploid = (1 if chr_i != 'chrX' and chr_j != 'chrX' else 2)
+            hic_m = cf.matrix(balance=False).fetch(chr_i,chr_j)
+            for ki,clu_i in enumerate(clusters[chr_i]):
+                for kj,clu_j in enumerate(clusters[chr_j]):
+                    print "{}:{},{}:{}".format(chr_i,ki,chr_j,kj)
+                    sumv = np.sum(hic_m[clu_i,:][:,clu_j]) * ploid
+                    mat[offset[chr_i]+ki,offset[chr_j]+kj] = mat[offset[chr_j]+kj,offset[chr_i]+ki] = sumv
+                    normat[offset[chr_i]+ki,offset[chr_j]+kj] = normat[offset[chr_j]+kj,offset[chr_i]+ki] = sumv/float(len(clu_i))/float(len(clu_j))
+
+    N = normat.shape[0]
+    w, v = scipy.linalg.eigh(normat, eigvals=(N-k,N-1))
+
+    gmix = mixture.GaussianMixture(n_components=k, covariance_type='full', tol=1e-4, max_iter=1000)
+    gmix.fit(v)
+    gmm_clust = gmix.predict(v)
+
+    clu_idx = np.argsort(gmm_clust)
+    P = np.zeros(normat.shape)
+    P[np.arange(0,len(clu_idx)),clu_idx] = 1
+    # Permute rows and columns.
+    W_clust = np.dot(np.dot(P,normat),np.linalg.inv(P))
+    plt.matshow(W_clust,cmap=plt.cm.bwr)
+
+    clust_cnt = [(g[0], len(list(g[1]))) for g in itertools.groupby(sorted(gmm_clust))]
+    # Compute cluster limits
+    cnt = np.zeros(k+1,dtype=int)
+    for i in xrange(k):
+        cnt[i] += clust_cnt[i][1]
+    cmcnt = np.cumsum(cnt)
+    l = W_clust.shape[0]-1
+    for x in cmcnt:
+        plt.plot([0,l], [x,x], color='k', linestyle='-', linewidth=1)
+        plt.plot([x,x], [0,l], color='k', linestyle='-', linewidth=1)
+
+    with open('global_clusters.out','w+') as fo:
+        c_id = 0
+        for i in xrange(0,len(clusters)):
+            chr_i = clusters.keys()[i]
+            for ki,clu_i in enumerate(clusters[chr_i]):
+                fo.write("{}\t{}\t{}\t{}\n".format(clusters.keys()[i],ki,gmm_clust[c_id],','.join([str(n) for n in clu_i])))
+                c_id += 1
+    
+    # Now normalize by cluster size.
+    # e.g. chr1 with 15 chr will have more bins in each chr than chr22. Hence, we must normalize by row/column. So that each cell has a unit of contacts/bin^2.
+    
+
+def cluster_compartments(cf,k,chrlist,eig_dim=None,contact_thr=1,corr_diags=0,max_sample_size=50000,coeffs=None,coeffs_gw=None,seed=None,max_resampling_attempts=10,rearrange_clusters=False,use_ice=False,tsne_precluster=True):
+    if not use_ice:
+        if coeffs is None and coeffs_gw is None:
+            print 'computing coeffs...'
+            coeffs = oe_coeffs_mask(cf,cf.chromnames)
+        elif coeffs is None and coeffs_gw is not None:
+            print 'using GW coeffs'
+    else:
+        print 'using ICE balancing...'
+
+    if eig_dim == None:
+        eig_dim = 2*k
+
+    clusters = {}
+    sample_idx = {}
+    clusters_idx = {}
+
+    for chr in chrlist:
+        if os.path.isfile('clusters_{}.txt'.format(chr)):
+            print "cluster file for {} (clusters_{}.txt) already exists. Skipping chromosome.".format(chr,chr)
+            continue
+            
+        if not use_ice:
+            m = cf.matrix(balance=False).fetch(chr)
+            # Threshold contacts
+            m[np.where(m<contact_thr)] = 0
+            if coeffs_gw is not None:
+                m_oe = oe_apply(m,coeffs_gw).toarray()
+            else:
+                m_oe = oe_apply(m,coeffs[chr]).toarray()
+        else:
+            m_oe = cf.matrix(balance=True).fetch(chr)
+            
+        # Get idx of high quality regions (measured in raw matrix).
+        samp_idx = matrix_mask_idx(m_oe)
+        sample_idx[chr] = samp_idx
+        print "{} (matrix rows: {}, max sample: {})".format(chr,m.shape[0],samp_idx.shape[0])
+        # High-quality matrix size
+        l = len(samp_idx)
+        ssize = min(l,max_sample_size)
+        # Sample iteration (keep sampling while clustering fails).
+        np.random.seed(seed)
+        successful = False
+        cnt = 0
+        while not successful and cnt < max_resampling_attempts:
+            cnt += 1
+            # Get sample
+            if ssize < l:
+                s = np.sort(np.random.choice(samp_idx,ssize,replace=False))
+            else:
+                s = np.array(samp_idx)
+            m_samp = m_oe[s,:][:,s]
+            # Relax outliers
+            m_max = np.percentile(m_samp[np.where(m_samp>0)],90)
+            m_samp[np.where(m_samp > m_max)] = m_max
+            if (~m_samp.any(axis=1)).any():
+                print "sample contains all-0 rows. resampling..."
+                continue
+                
+            # Remove diagonals before correlation
+            if corr_diags > 0:
+                m_cor = np.corrcoef(np.triu(m_samp,corr_diags) + np.tril(m_samp,-corr_diags))
+            else:
+                m_cor = np.corrcoef(m_samp)
+
+            # Remove correlation diagonal
+            np.fill_diagonal(m_cor,0)
+
+            # Increase correlation contrast (5-95 percentiles)
+            #m_cor = m_cor - np.median(m_cor[np.triu_indices(ssize,1)])
+            min_cor_val = np.percentile(m_cor[np.triu_indices(ssize,1)],5)
+            max_cor_val = np.percentile(m_cor[np.triu_indices(ssize,1)],95)
+            m_cor[np.where(m_cor < min_cor_val)] = min_cor_val
+            m_cor[np.where(m_cor > max_cor_val)] = max_cor_val
+
+            # Spectral clustering
+            N = m_cor.shape[0]
+            eig_dim = min(N,eig_dim)
+            try:
+                print "spectral clustering..."
+                w, v = scipy.linalg.eigh(m_cor, eigvals=(N-eig_dim,N-1))
+                if tsne_precluster:
+                    print "t-SNE to k=3..."
+                    v_clust = manifold.TSNE(n_components=3).fit_transform(v)
+                else:
+                    v_clust = v
+                # Cluster eigenvectors using Gaussian Mixture
+                print "cluster identification with GaussianMixture..."
+                gmix = mixture.GaussianMixture(n_components=k, covariance_type='full', tol=1e-4, max_iter=1000)
+                gmix.fit(v_clust)
+                gmm_clust = gmix.predict(v_clust)
+                # TODO: Try t-sne instead of GaussMix
+            except:
+                print "error while clustering. resampling..."
+                continue
+            successful = True
+
+        if cnt >= max_resampling_attempts:
+            print "max reampling attempts reached, skipping chromosome."
+            continue
+
+        # Rearrange clusters for visualization
+        # Make cluster index list
+        clu_idx = [list() for _ in xrange(k)]
+        for i,c in enumerate(gmm_clust):
+            clu_idx[c].append(i)
+
+        if not rearrange_clusters:
+            # Map again to matrix indices
+            clusters_idx[chr] = [sample_idx[chr][x] for x in clu_idx]
+
+        else:
+            for i in xrange(k):
+                clu_idx[i] = np.array(clu_idx[i])
+
+            clusters[chr] = list()
+
+            # Find most distant blocks
+            l_r = (0,0)
+            val = np.inf
+            d_sum = np.zeros((k,k))
+            for i in xrange(k):
+                l_i = len(clu_idx[i])
+                for j in xrange(i+1,k):
+                    l_j = len(clu_idx[j])
+                    d_sum[i,j] = np.sum(m_cor[clu_idx[i],:][:,clu_idx[j]])
+                    d = float(d_sum[i,j])/(l_i*l_j)
+                    if d < val:
+                        l_r = (i,j)
+                        val = d
+
+            # Pop left and right blocks (important to do it in this order for index consistency).
+            r_idx = clu_idx.pop(l_r[1])
+            l_idx = clu_idx.pop(l_r[0])
+            r_clusters = [r_idx.copy(),]
+            l_clusters = [l_idx.copy(),]
+
+            iters = len(clu_idx)/2 + len(clu_idx)%2
+            for i in xrange(iters):
+                # Find nearest blocks to L/R.
+                len_l = len(l_idx)
+                len_r = len(r_idx)
+                min_d = np.inf
+                max_d = -np.inf
+                min_idx = 0
+                max_idx = 0
+                for i in xrange(len(clu_idx)):
+                    len_block = len(clu_idx[i])
+                    d_l = float(np.sum(m_cor[l_idx,:][:,clu_idx[i]]))/(len_l*len_block) - val
+                    d_r = float(np.sum(m_cor[r_idx,:][:,clu_idx[i]]))/(len_r*len_block) - val
+                    r = d_l/d_r
+                    if r < min_d:
+                        min_idx = i
+                        min_d = r
+                    if r >= max_d:
+                        max_idx = i
+                        max_d = r
+                # Pop from idx and add to L/R.
+                if min_idx > max_idx:
+                    r_clusters.append(clu_idx[min_idx].copy())
+                    l_clusters.append(clu_idx[max_idx].copy())
+                    r_idx = np.append(clu_idx.pop(min_idx),r_idx)
+                    l_idx = np.append(l_idx,clu_idx.pop(max_idx))
+                elif min_idx < max_idx:
+                    r_clusters.append(clu_idx[min_idx].copy())
+                    l_clusters.append(clu_idx[max_idx].copy())
+                    l_idx = np.append(l_idx,clu_idx.pop(max_idx))
+                    r_idx = np.append(clu_idx.pop(min_idx),r_idx)
+                else:
+                    l_clusters.append(clu_idx[max_idx].copy())
+                    l_idx = np.append(l_idx,clu_idx.pop(max_idx))
+            # Make final index list.
+            clu_idx = np.append(l_idx,r_idx)
+
+            # Make final cluster index list.
+            clusters[chr] = l_clusters + list(reversed(r_clusters))
+
+            # Map again to matrix indices
+            clusters_idx[chr] = [sample_idx[chr][x] for x in clusters[chr]]
+
+        # Store in disk
+        fout = open('clusters_{}.txt'.format(chr),'w+')
+        for c in clusters_idx[chr]:
+            fout.write("{}\t".format(chr))
+            fout.write(','.join([str(i) for i in c]))
+            fout.write('\n')
+            
+        fout.close()
+
+        '''
+        # Make permutation matrix
+        P = np.zeros(m_cor.shape)
+        P[np.arange(0,len(clu_idx)),clu_idx] = 1
+
+        # Permute rows and columns.
+        W_clust = np.dot(np.dot(P,m_cor),np.linalg.inv(P))
+        plt.matshow(W_clust,cmap=plt.cm.bwr)
+
+        # Compute cluster limits
+        cnt = np.zeros(k+1,dtype=int)
+        for i in xrange(len(clusters[chr])):
+            cnt[i] += len(clusters[chr][i])
+        cnt = np.cumsum(cnt)
+        l = W_clust.shape[0]-1
+        for x in cnt:
+            plot([0,l], [x,x], color='k', linestyle='-', linewidth=1)
+            plot([x,x], [0,l], color='k', linestyle='-', linewidth=1)
+        title('{} cluster k={}'.format(chr,k))
+        '''
+
+    return clusters_idx, coeffs
+
         
+def interchromosomal_cluster_matrix(cf,clusters_idx,c1,c2):
+    # Interchromosomal cluster matrix
+    # clusters_idx['chr'] contains the cluster indices.
+    # Read matrix
+    m = cf.matrix(balance=False).fetch(c1,c2)
+    # Compute cluster lengths
+    len_i = [len(x) for x in clusters_idx[c1]]
+    len_j = [len(x) for x in clusters_idx[c2]]
+
+    # Compute interchromosomal matrix with cluster delimiters
+    allidx_i = np.array([idxset for clust in clusters_idx[c1] for idxset in clust])
+    allidx_j = np.array([idxset for clust in clusters_idx[c2] for idxset in clust])
+    m_ic = m[allidx_i,:][:,allidx_j]
+
+    # Plot matrix with cluster delimiters
+    plt.matshow(m_ic,cmap=plt.cm.Reds)
+
+    cnt_i = np.cumsum(len_i)
+    cnt_j = np.cumsum(len_j)
+    m_rows = m_ic.shape[0]
+    m_cols = m_ic.shape[1]
+    for x in cnt_i:
+        plot([0,m_cols], [x,x], color='k', linestyle='-', linewidth=1)
+    for x in cnt_j:
+        plot([x,x], [0,m_rows], color='k', linestyle='-', linewidth=1)
+
+    # Compute interchromosomal cluster matrix
+    clusts_i = len(clusters_idx[c1])
+    clusts_j = len(clusters_idx[c2])
+    icm = np.zeros((clusts_i,clusts_j))
+    for i,idx_i in enumerate(clusters_idx[c1]):
+        for j,idx_j in enumerate(clusters_idx[c2]):
+            icm[i,j] = float(sum(m[idx_i,:][:,idx_j]))/(len(idx_i)*len(idx_j))
+
+    plt.matshow(icm)
 
 def cpb_step(m):
     # Matrix cpb balancing.
-    its = m.sum(axis=1)        # Total interaction signal
-    itc = (m>0).sum(axis=1) # Number of interactions
-    cpb = its/(itc.astype('f'))
-    cpb = np.asarray(cpb).reshape(cpb.shape[0],)
+    its = np.asarray(np.sum(m,0)).reshape(-1)        # Total interaction signal
+    itc = np.asarray((m>0).sum(axis=0)).reshape(-1) # Number of interactions
+    cpb = np.ones(len(its))
+    idx = np.where(its>0)
+    cpb[idx] = its[idx]/(itc[idx].astype('f'))
     # Normalize CPB
     cpb_notnan = cpb[np.where(np.logical_not(np.isnan(cpb)))]
     cpb = cpb/np.max(cpb_notnan)
@@ -73,6 +412,95 @@ def oe_apply(m, coeffs):
         matrix.data[i] = matrix.data[i] / coeffs[abs(matrix.col[i]-matrix.row[i])]
 
     return matrix
+
+def matrix_mask_idx(m, bsm_lambda = lambda x: np.median(x)/2, cpb_lambda = lambda x: np.median(x)/2):
+        # Mask low quality regions.
+        rsm = np.sum(m,1)
+        bsm = np.sum(np.sign(m),1)
+        cpb = rsm/bsm
+
+        bsm_idx = np.where(bsm >= bsm_lambda(bsm))[0]
+        cpb_idx = np.where(cpb >= cpb_lambda(cpb))[0]
+
+        # Mask low quality 
+        return np.unique(np.sort(np.append(bsm_idx,cpb_idx)))
+
+def nan_helper(y):
+    return np.isnan(y), lambda z: z.nonzero()[0]
+
+def oe_coeffs_mask(cfile, ranges, bsm_lambda = lambda x: np.median(x)/2, cpb_lambda = lambda x: np.median(x)/2):
+    '''
+    Returns a dictionary with O/E normalization coefficients for each chromosome.
+    Before computing O/E, some regions of the matrix are masked and excluded.
+    Matrix row/columns are masked following the lambda functions:
+    bsm_lambda: excludes all rows with bin sum (bins with at least 1 contact) smaller than the value returned by bsm_labmda.
+    cpb_lambda: excludes all rows with contact per bin (average contact per row) smaller than the value returned by cpb_labmda.
+    Dictionary keys are chromosome names.
+    '''
+    if type(ranges) is not list:
+        ranges = [ranges,]
+
+    pr_int = {}
+    # Compute observed interactions for selected chromosomes
+    chrlist = []
+    chrbins = []
+    for cname in ranges:
+        chrname = cname.split(':')[0]
+        chrlist.append(chrname)
+        chrbins.append(cfile.extent(str(chrname)))
+
+    for c,bins in zip(chrlist,chrbins):
+        # Compute observed interactions for each chromosomes
+        m = cfile.matrix(balance=False).fetch(c)
+
+        # Mask low quality regions.
+        rsm = sum(m,1)
+        bsm = sum(np.sign(m),1)
+        cpb = rsm/bsm
+
+        bsm_idx = np.where(bsm < bsm_lambda(bsm))[0]
+        cpb_idx = np.where(cpb < cpb_lambda(cpb))[0]
+
+        # Mask low quality 
+        idx = np.unique(np.sort(np.append(bsm_idx,cpb_idx)))
+        m[idx,:] = 0
+        m[:,idx] = 0
+
+        # Convert matrix to sparse
+        m = scipy.sparse.coo_matrix(m)
+
+        # ObI: Observed Interactions
+        # PrI: Probability of Interaction
+        max_int_dist = m.shape[0]
+        obs_int = [0]*(max_int_dist)
+        pr_int[c] = np.zeros(max_int_dist,dtype=float)
+
+        for i,j,v in zip(m.row, m.col, m.data):
+            obs_int[abs(i-j)] += v
+
+        for int_dist in range(0,max_int_dist):
+            if not obs_int[int_dist]:
+                pr_int[c][int_dist] = float('NaN')
+                continue
+            # PsI: Possible interactions
+            possible_int = max_int_dist-int_dist
+            # Correct masked rows
+            tmp = np.append(idx[np.where(idx >= int_dist)], idx + int_dist)
+            tmp = tmp[np.where(tmp < max_int_dist)]
+            possible_int -= len(np.unique(tmp))
+            # Probability:
+            if possible_int != 0:
+                pr_int[c][int_dist] = obs_int[int_dist] / float(possible_int)
+            else:
+                pr_int[c][int_dist] = float('NaN')
+
+        # Interpolate NaN values.
+        nans, x= nan_helper(pr_int[c])
+        if len(nans) > 0:
+            pr_int[c][nans]= np.interp(x(nans), x(~nans), pr_int[c][~nans])
+
+    return pr_int
+
     
 def oe_coeffs_chrom(cfile, ranges):
     '''
